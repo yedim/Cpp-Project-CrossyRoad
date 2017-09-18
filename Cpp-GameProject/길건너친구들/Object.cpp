de343b086af4e122611855a034f753702f380228
yedim <yelimi0416@gmail.com>
2017-09-19 03:10:40 +0900

    Object
    장애물과 플레이어에서 동시에 발생하는 함수정의.

---
+++ Cpp-GameProject/길건너친구들/Object.cpp
@@ -0,0 +1,67 @@
+#include "Object.h"
+#include "Obstacle.h"
+#include "Player.h"
+
+CObject::CObject()
+{
+
+}
+CObject::~CObject()
+{
+
+}
+
+void CObject::AddObject(CObstacle* obstacle, int x, int y)
+{
+	obstacle->Init(x, y);
+	obstacle->x = x;
+	obstacle->y = y;
+	ObstacleList[obstacle->GetType()].push_back(obstacle);
+}
+
+void CObject::Move(CPlayer* player)
+{
+	for (int i = 0; i < OBJ_MAX; ++i)
+	{
+		for (list<CObstacle*>::iterator iter = ObstacleList[i].begin(); iter != ObstacleList[i].end();)
+		{
+			(*iter)->Move();
+
+			//충돌했을 때 조건
+			if ((*iter)->GetX() < player->player_x + player->width &&//왼쪽
+				(*iter)->GetX() + (*iter)->GetWidth() > player->player_x &&//오른쪽
+				(*iter)->y + (*iter)->GetHeight() > player->player_y &&//아래
+				(*iter)->y < player->player_y + player->height)//위
+			{
+				cout << "충돌!!!";
+			}
+
+			//화면을 벗어나면 리스트의 객체를 지운다
+			if ((*iter)->y + (*iter)->GetHeight() >= SCREEN_Y && (*iter)->GetX() + (*iter)->GetWidth() >= SCREEN_X - 1)
+			{
+				//delete (*iter);
+				//(*iter) = NULL;
+				iter = ObstacleList[(*iter)->GetType()].erase(iter);
+			}
+			else
+			{
+				iter++;
+			}
+		}
+	}
+}
+
+void CObject::Scroll()
+{
+	for (int i = 0; i < OBJ_MAX; ++i)
+	{
+		for (list<CObstacle*>::iterator iter = ObstacleList[i].begin(); iter != ObstacleList[i].end(); ++iter)
+		{
+			//화면 밖으로 자동차가 나가지 않도록 함.
+			if ((*iter)->y + (*iter)->GetHeight() < SCREEN_Y)
+			{
+				(*iter)->Init((*iter)->y += 3);
+			}
+		}
+	}
+}
